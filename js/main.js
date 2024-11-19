fe6d2cbfdcf0ba395adc85532329493411141db6
Ariana Gonzabay <arigalda@espol.edu.ec>
2024-11-18 23:20:46 -0500

    Cambio del contenido para la pagina de Comida

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const databaseURL = 'https://landing-b5875-default-rtdb.firebaseio.com/coleccion.json';
 /*FUNCIONES FLECHA*/
+
 let sendData = ( ) => {  
 
     // Obtén los datos del formulario
@@ -24,6 +25,10 @@ let sendData = ( ) => {
     .then(result => {
         alert('Agradeciendo tu preferencia, nos mantenemos actualizados y enfocados en atenderte como mereces'); // Maneja la respuesta con un mensaje
         form.reset()
+
+        //recuperación de datos
+        getData();
+
     })
     .catch(error => {
         alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
@@ -31,9 +36,75 @@ let sendData = ( ) => {
   
 }
 
+let getData = async () => {  
+
+  try {
+
+      // Realiza la petición fetch a la URL de la base de datos
+      const response = await fetch(databaseURL, {
+        method: 'GET'
+      });
+
+      // Verifica si la respuesta es exitosa
+      if (!response.ok) {
+        alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+      }
+
+      // Convierte la respuesta en formato JSON
+      const data = await response.json();
+
+      if(data != null) {
+
+        // Cuente el número de suscriptores registrados por fecha a partir del objeto data
+        let countSuscribers = new Map();
+        if (Object.keys(data).length > 0){
+          for(let key in data){
+            let {email, saved} = data[key];
+
+            let date = saved.split(",")[0]
+
+            let count = countSuscribers.get(date) || 0;
+            countSuscribers.set(date, count + 1)
+          }
+        }
+
+        // END
+
+          // Genere y agregue filas de una tabla HTML para mostrar fechas y cantidades de suscriptores almacenadas 
+          
+          if (countSuscribers.size > 0) {
+
+
+            subscribers.innerHTML = ''
+
+            let index = 1;
+            for (let [date, count] of countSuscribers) {
+              let rowTemplate = `
+              <tr>
+                  <th>${index}</th>
+                  <td>${date}</td>
+                  <td>${count}</td>
+              </tr>`
+                subscribers.innerHTML += rowTemplate
+                index++;
+            }
+          }
+          // END
+
+      }
+
+    } catch (error) {
+      // Muestra cualquier error que ocurra durante la petición
+      alert('Hemos experimentado un error. ¡Vuelve pronto!'); // Maneja el error con un mensaje
+    }
+
+}
+
 let ready = () => {
     console.log('DOM está listo')
     //debugger
+    //recuperacion de datos
+    getData();
   }
   
   let loaded = ( eventLoaded ) => {
